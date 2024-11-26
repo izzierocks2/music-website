779e4123c0bcd57ecb0ebf7f5bc1a21d7ed31f88
Grace <gracek2705@tuckahoeschools.org>
2024-11-26 13:55:10 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const likeButton1 = document.getElementById('like-button1');
 const dislikeButton1 = document.getElementById('dislike-button1');
 let score = 0;
+
+document.getElementById("myButton").addEventListener("click", () =>{
+    document.getElementById("myImage").style.display = "block";
+});        //button displays image when clicked
